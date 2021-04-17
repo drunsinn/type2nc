4d3415ad4f6d331a3400e14f892371af6d69d955
drunsinn <max@chp1.net>
2021-04-17 11:25:05 +0200

    Update type2nc.py
    - add creating of demo files
    - add logging to ui
    - change scale factor calculation

--- type2nc/type2nc.py
+++ type2nc/type2nc.py
@@ -8,6 +8,7 @@ import datetime
 import logging
 import argparse
 import pathlib
+import math
 import string
 import freetype
 import numpy as np
@@ -39,12 +40,12 @@ class Type2NC(object):
         self._log.debug("running for font file '%s'", font_file.resolve(strict=True))
 
         font_face = freetype.Face(str(font_file.resolve(strict=True)))
-        self._log.debug("Font: %s, Style: %s, Format: %s", font_face.family_name.decode("utf-8"), font_face.style_name.decode("utf-8"), font_face.get_format().decode("utf-8"))
+        self._log.info("Font: %s, Style: %s, Format: %s", font_face.family_name.decode("utf-8"), font_face.style_name.decode("utf-8"), font_face.get_format().decode("utf-8"))
         font_face.set_char_size(height=self.__char_size)
         
-        self._log.debug("Font BBox (min:max) X:(%d:%d) Y:(%d:%d)", font_face.bbox.xMax, font_face.bbox.xMin, font_face.bbox.yMax, font_face.bbox.yMin)
-        scale_factor = self.__target_height / font_face.bbox.xMax
-        self._log.debug("set scaling factor to %f", scale_factor)
+        self._log.info("Font BBox (min:max) X:(%d:%d) Y:(%d:%d)", font_face.bbox.xMax, font_face.bbox.xMin, font_face.bbox.yMax, font_face.bbox.yMin)
+        scale_factor = self.__target_height / (abs(font_face.bbox.xMax) + abs(font_face.bbox.xMin))
+        self._log.info("set scaling factor to %f", scale_factor)
 
         nc_file_name = font_file.name.replace(font_file.suffix, '.H')
         nc_file_name = nc_file_name.replace(' ', '_')
@@ -222,13 +223,42 @@ class Type2NC(object):
         else:
             label_name = None  # could not identify character
         return label_name
+    
+    def generate_demo_files(self, font_file_list):
+        self._log.debug("create demo files")
+        filler = ""
+        current_y = 5
+        for font_file in font_file_list:
+            nc_file_name = font_file.name.replace(font_file.suffix, '.H')
+            nc_file_name = nc_file_name.replace(' ', '_')
 
+            filler += "L X+5 Y+%d Z+100 R0 FMAX\n" % current_y
+            filler += "CALL PGM %s\n;\n" % nc_file_name
+            current_y += self.__target_height + 5
+        filler = filler.rstrip("\n")
+
+        block_heigth = current_y + 2 * self.__target_height
+            
+        with open(pathlib.Path.cwd().joinpath("templates", "demo_pgm_template_conventional.H")) as template_file:
+            with open(self.__output_folder.joinpath("type2nc_demo_conventional.H"), 'w') as output_file:
+                demo_file_content = template_file.read()
+                output_file.write(demo_file_content.format(block_heigth, filler))
+        self._log.debug("demo file for conventional calling written successfully")
+    
+        with open(pathlib.Path.cwd().joinpath("templates", "demo_pgm_template_cycle.H")) as template_file:
+            with open(self.__output_folder.joinpath("type2nc_demo_cycle.H"), 'w') as output_file:
+                demo_file_content = template_file.read()
+                output_file.write(demo_file_content.format(block_heigth, filler))
+        self._log.debug("demo file for cycle based calling written successfully")
+    
 class Type2NC_UI:
     def __init__(self, root):
+        self._log = logging.getLogger("Type2NCUI")
+        self._log.debug("start building tk ui")
         self._window_root = root
         root.title("type2nc")
         width=400
-        height=210
+        height=250
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
@@ -237,66 +267,88 @@ class Type2NC_UI:
 
         ft = tkFont.Font(family='Times',size=12)
 
-        self.btn_select_font=tk.Button(self._window_root)
-        
+        current_y = 20
+        delta_y = 40
+        component_height = 30
+
+        self.btn_select_font = tk.Button(self._window_root)
         self.btn_select_font["font"] = ft
         self.btn_select_font["justify"] = "center"
         self.btn_select_font["text"] = "Select Font File"
-        self.btn_select_font.place(x=20, y=20, width=160, height=30)
+        self.btn_select_font.place(x=20, y=current_y, width=160, height=component_height)
         self.btn_select_font["command"] = self.btn_select_font_command
 
-        self.lbl_font_filename=tk.Label(self._window_root)
+        self.lbl_font_filename = tk.Label(self._window_root)
         self.lbl_font_filename["font"] = ft
         self.lbl_font_filename["justify"] = "center"
         self.lbl_font_filename["text"] = " "
-        self.lbl_font_filename.place(x=200, y=20, width=180, height=30)
+        self.lbl_font_filename.place(x=200, y=current_y, width=180, height=component_height)
+        current_y += delta_y
 
-        self.btn_select_folder=tk.Button(self._window_root)
+        self.btn_select_folder = tk.Button(self._window_root)
         self.btn_select_folder["font"] = ft
         self.btn_select_folder["justify"] = "center"
         self.btn_select_folder["text"] = "Select destination Folder"
-        self.btn_select_folder.place(x=20, y=60, width=160, height=30)
+        self.btn_select_folder.place(x=20, y=current_y, width=160, height=component_height)
         self.btn_select_folder["command"] = self.btn_select_folder_command
 
-        self.lbl_output_path=tk.Label(self._window_root)
+        self.lbl_output_path = tk.Label(self._window_root)
         self.lbl_output_path["font"] = ft
         self.lbl_output_path["justify"] = "center"
         self.lbl_output_path["text"] = " "
-        self.lbl_output_path.place(x=200, y=60, width=180, height=30)
+        self.lbl_output_path.place(x=200, y=current_y, width=180, height=component_height)
+        current_y += delta_y
 
-        self.btn_select_step=tk.Button(self._window_root)
+        self.btn_select_step = tk.Button(self._window_root)
         self.btn_select_step["font"] = ft
         self.btn_select_step["justify"] = "center"
         self.btn_select_step["text"] = "Select step size"
-        self.btn_select_step.place(x=20, y=100, width=160, height=30)
+        self.btn_select_step.place(x=20, y=current_y, width=160, height=component_height)
         self.btn_select_step["command"] = self.btn_select_step_command
 
-        self.lbl_step_size=tk.Label(self._window_root)
+        self.lbl_step_size = tk.Label(self._window_root)
         self.lbl_step_size["font"] = ft
         self.lbl_step_size["justify"] = "center"
         self.lbl_step_size["text"] = " "
-        self.lbl_step_size.place(x=200, y=100, width=180, height=30)
-        
-        self.btn_generate_nc=tk.Button(self._window_root)
+        self.lbl_step_size.place(x=200, y=current_y, width=180, height=component_height)
+        current_y += delta_y
+
+        self.gen_demo_files = tk.IntVar()
+        self.chk_generate_demos = tk.Checkbutton(self._window_root)
+        self.chk_generate_demos["font"] = ft
+        self.chk_generate_demos["justify"] = "left"
+        self.chk_generate_demos["text"] = "Create demo files"
+        self.chk_generate_demos.place(x=20, y=current_y, width=150, height=component_height)
+        self.chk_generate_demos["onvalue"] = 1
+        self.chk_generate_demos["offvalue"] = 0
+        self.chk_generate_demos.select()
+        self.chk_generate_demos["variable"] = self.gen_demo_files
+        current_y += delta_y
+
+        self.btn_generate_nc = tk.Button(self._window_root)
         self.btn_generate_nc["font"] = ft
         self.btn_generate_nc["justify"] = "center"
         self.btn_generate_nc["text"] = "generate"
-        self.btn_generate_nc.place(x=140, y=140, width=150, height=30)
+        self.btn_generate_nc.place(x=140, y=current_y, width=150, height=component_height)
         self.btn_generate_nc["command"] = self.btn_generate_nc_command
+        current_y += delta_y
 
         self.pb_progress = ttk.Progressbar(self._window_root)
         self.pb_progress["orient"] = tk.HORIZONTAL
         self.pb_progress["length"] = 100
-        self.pb_progress.place(x=115, y=170, width=200, height=30)
+        self.pb_progress.place(x=115, y=current_y, width=200, height=component_height)
         self.pb_progress["mode"] = 'determinate'
         self.pb_progress['value'] = 0
+        current_y += delta_y
+
+        self._log.debug("ui ready")
 
         self.selected_font_files = list()
         self.output_path = None
         self.step_size = None
 
-
     def btn_select_font_command(self):
+        self._log.debug("select font button pressed")
         file_types = [('Font', '*.ttf *.tte *.ttc *.otf *.dfont *.pfb')]
         font_file_list = tkfd.askopenfilename(parent=self._window_root, title="Select Font file", filetypes=file_types, multiple=1)
         for path in font_file_list:
@@ -304,47 +356,67 @@ class Type2NC_UI:
 
         if len(self.selected_font_files) == 1:
             self.lbl_font_filename["text"] = str(self.selected_font_files[0].name)
+            self._log.debug("No font file selected")
         else:
             self.lbl_font_filename["text"] = "%d files selected" % len(self.selected_font_files)
+            self._log.debug("Add %d fonts to list: %s", len(font_file_list), font_file_list)
 
     def btn_select_folder_command(self):
+        self._log.debug("select output folder button pressed")
         selected_folder = tkfd.askdirectory(parent=self._window_root, title="Select destination folder")
         self.output_path = pathlib.Path(selected_folder)
 
         if not self.output_path.is_dir():
             self.output_path = None
             self.lbl_output_path["text"] = "No folder selected"
+            self._log.debug("No folder selected")
         else:
             self.lbl_output_path["text"] = self.output_path.name
+            self._log.debug("Output folder set to %s", self.output_path)
 
     def btn_select_step_command(self):
+        self._log.debug("select step size button pressed")
         selected_step = tksd.askfloat(parent=self._window_root, title="Step Size", prompt="Step Size between 0.001 (very fine) and 0.2 (very coarse) for converting Splines", initialvalue=0.05, minvalue=0.001, maxvalue=0.2)
         if selected_step is None:
             self.step_size = None
             self.lbl_step_size["text"] = "No step size selected"
+            self._log.debug("No step size selected")
         else:
             self.step_size = selected_step
             self.lbl_step_size["text"] = "%f" % self.step_size
+            self._log.debug("step size set to %f", self.step_size)
 
     def btn_generate_nc_command(self):
+        self._log.debug("generate nc files button pressed")
         if len(self.selected_font_files) == 0:
             tkmb.showwarning(parent=self._window_root, title="Missing parameter", message="No font files selected")
+            self._log.debug("list of selected font files is empty!")
             return
         if self.output_path is None:
             tkmb.showwarning(parent=self._window_root, title="Missing parameter", message="No output folder selected")
+            self._log.debug("no output path selected!")
             return
         if self.step_size is None:
             tkmb.showwarning(parent=self._window_root, title="Missing parameter", message="No step size selected")
+            self._log.debug("no step size selected selected!")
             return
 
         self.pb_progress['value'] = 0
         conv = Type2NC(output_folder=self.output_path, target_height=10, step_size=self.step_size)
+        self._log.debug("start processing %d font files", len(self.selected_font_files))
         for i, ff in enumerate(self.selected_font_files):
             if ff.is_file():
                 conv.convert(ff)
             self.pb_progress['value'] = (i + 1) * (100 / len(self.selected_font_files))
 
+        if self.gen_demo_files.get() > 0:
+            self._log.debug("generating demo files is enabled")
+            conv.generate_demo_files(self.selected_font_files)
+
         tkmb.showwarning(parent=self._window_root, title="Finished", message="Finished processing all font files")
+
+        self.lbl_font_filename["text"] = " "
+        del self.selected_font_files[:]
 
 if __name__ == "__main__":
     logging.basicConfig(encoding='utf-8', level=logging.INFO)
@@ -355,7 +427,7 @@ if __name__ == "__main__":
     cmdl_parser.add_argument("-i", "--input", metavar="font input file", nargs='+', type=pathlib.Path, help="path of one or more font files")
     cmdl_parser.add_argument("-o", "--output", metavar="output folder", type=pathlib.Path, help="path to the output folder where klartext files are generated")
     cmdl_parser.add_argument("-s", "--step_size", metavar="step size", type=float, default=0.05, required=False, help="step size for converting curves to line segmenst: between 0.001 (very fine) and 0.2 (very coarse)")
-
+    cmdl_parser.add_argument("-d", "--create_demos", action='store_true', default=False, required=False, help="if set, demo output will use cycle 225 for definition of parameters")
     arguments = cmdl_parser.parse_args()
 
     if arguments.input is not None:
@@ -367,6 +439,9 @@ if __name__ == "__main__":
             if ff.is_file():
                 
                 conv.convert(ff)
+        
+        if arguments.create_demos:
+            conv.generate_demo_files(arguments.input)
     else:
         import tkinter.filedialog as tkfd
         import tkinter.simpledialog as tksd
